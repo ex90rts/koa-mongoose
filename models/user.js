d053ae53f9bf5b559e794cbf8b0df3b4385851f8
Alan Zhang <ex90rts@gmail.com>
2018-03-27 17:27:10 +0800

    comments to English

--- models/user.js
+++ models/user.js
@@ -7,9 +7,9 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//type:字段类型，包括String,Number,Date,Buffer,Boolean,Mixed,ObjectId,Array
-//index:是否索引，注意唯一索引unique的写法
-//default:默认值
+//type: filed type, String,Number,Date,Buffer,Boolean,Mixed,ObjectId,Array
+//index: is database index needed for the filed, pay attention to unique index
+//default: the default value
 const User = new Schema({
     "username": { type: String, index:  {unique: true, dropDups: true} },
     "password": { type: String, match: /\w+/, index: true },
@@ -19,26 +19,26 @@ const User = new Schema({
     "updated": { type: Date, default: Date.now, index: true },
 });
 
-//使用setter，将用户输入的明文密码sha1之后存储
+// Use the setter feature to hash the password text before save, while it's for the safety.
 User.path('password').set(function(v){
     let shasum = crypto.createHash('sha1');
     shasum.update(v);
     return shasum.digest('hex');
 });
 
-//使用middleware，每次保存都记录一下最后更新时间
+// user middleware to auto update the field 'updated' while you changed any other things
 User.pre('save', function(next){
     this.updated = Date.now();
     next();
 });
 
-//静态方法，按用户名查找，因为username加了唯一索引，
-//所以这里用的是findOne，只查询一条数据
+// Static method: find an user by username value
+// use findOne method here becase we just need to query one record
 User.statics.findByUsername = function(username){
     return this.findOne({username: username});
 };
 
-//静态方法，按角色查找
+//Static method: find all user by roles
 User.statics.findByRoles = function(roles){
     if (!Array.isArray(roles)){
         roles = [roles];
@@ -46,27 +46,27 @@ User.statics.findByRoles = function(roles){
     return this.find({roles:{$in:roles}});
 };
 
-//静态方法，按负责的杂志查找
+//Static method: find all user by magazine
 User.statics.findByMagazine = function(magazine){
     return this.find({"magazines.name": magazine});
 };
 
-//静态方法，按负责的杂志类型查找
+//Static method: find all user by magazine type
 User.statics.findByMagazineType = function(magazineType){
     return this.find({"magazines.type": magazineType});
 };
 
-//静态方法，查找(一个)没有被雇佣(即不是编辑、也不是作者)的人
+//Static method: find an user who doesn't have a job yet( not an editor nor an author)
 User.statics.findJobless = function(){
     return this.findOne({roles:{$nin:["editor", "author"]}});
 };
 
-//实例方法，判断是否是编辑
+// instance method, check if this user is an editor of current model instance
 User.methods.isEditor = function(){
     return this.roles.indexOf("editor")!==-1;
 };
 
-//创建模型
+// create the model
 const model = mongoose.model('User', User);
 
 module.exports = model;
